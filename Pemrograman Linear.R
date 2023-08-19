2aa565ca698ae46d35da2e7ee971a6e08e905dcd
I Gede Surya Rahayuda <surya.rahayuda@gmail.com>
2023-08-20 00:33:44 +0800

    Add files via upload

---
+++ Pemrograman Linear.R
@@ -0,0 +1,27 @@
+library(lpSolve)
+
+Keputusan <- c(20, 15)
+A <- matrix(c(1, 0.5, 
+              1, 1), nrow=2, byrow=TRUE)
+B <- c(30, 50)
+arah <- c("<=","<=")
+
+optimal <- lp(direction = "max",
+              objective.in = Keputusan,
+              const.mat = A,
+              const.dir = arah,
+              const.rhs = B,
+              all.int = T)
+
+str(optimal)
+
+solusiterbaik <- optimal$solution
+
+solusiterbaik
+
+names(solusiterbaik) <- c("Pohon Jeruk", "Pohon Apel")
+
+print(solusiterbaik)
+
+print(paste("Pendapatan maksimal yang didapatkan: ",optimal$objval,sep=""))
+
